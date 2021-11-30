edde7f688295f31113764188ab49e7de3aea994f
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-11-30 11:56:39 +0900

    ssigned values to book info variables

--- main.go
+++ main.go
@@ -6,5 +6,12 @@ func main() {
 	var publisher, writer, artist, title string
 	var year, pageNumber int
 	var grade float32
+	title = "Mr GoToSleep"
+	writer = "Tracey Hatchet"
+	artist = "Jewel Tampson"
+	publisher = "Dizzy Books Publishing Inc."
+	year = 1997
+	pageNumber = 14
+	grade = 6.5
 	fmt.Println(title, "published by", publisher, "written by", writer, "artist:", artist, "Year:", year, "pages:", pageNumber, "grade:", grade)
 }
