30b4b766950e9e9fc649fc1b9f9fe2f9143022cb
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-11-30 11:59:09 +0900

    added anouther book

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	var publisher, writer, artist, title string
 	var year, pageNumber int
 	var grade float32
+
 	title = "Mr GoToSleep"
 	writer = "Tracey Hatchet"
 	artist = "Jewel Tampson"
@@ -13,5 +14,14 @@ func main() {
 	year = 1997
 	pageNumber = 14
 	grade = 6.5
+	fmt.Println(title, "published by", publisher, "written by", writer, "artist:", artist, "Year:", year, "pages:", pageNumber, "grade:", grade)
+
+	title = "Epic Vol. 1"
+	writer = "Ryan N. Shawn"
+	artist = "Phoebe Paperclips"
+	publisher = "Dizzy Books Publishing Inc."
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
 	fmt.Println(title, "published by", publisher, "written by", writer, "artist:", artist, "Year:", year, "pages:", pageNumber, "grade:", grade)
 }
